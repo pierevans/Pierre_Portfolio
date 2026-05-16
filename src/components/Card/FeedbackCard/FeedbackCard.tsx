cf04f73090f58be2c45de8aa0e42b8ce26327526
pik8014 <pierre.konan@mavs.uta.edu>
2026-05-16 12:06:04 -0500

    feeedback is done

--- src/components/Card/FeedbackCard/FeedbackCard.tsx
+++ src/components/Card/FeedbackCard/FeedbackCard.tsx
@@ -1,62 +1,67 @@
 import { useState } from "react";
-import { FaRegFaceTired } from "react-icons/fa6";
-import { FaRegFaceFrown } from "react-icons/fa6";
-import { FaRegFaceMeh } from "react-icons/fa6";
-import { FaRegFaceSmileBeam } from "react-icons/fa6";
-import { FaRegFaceLaughSquint } from "react-icons/fa6";
+import {
+  FaRegFaceTired,
+  FaRegFaceFrown,
+  FaRegFaceMeh,
+  FaRegFaceSmileBeam,
+  FaRegFaceLaughSquint,
+} from "react-icons/fa6";
+import { IoCloseOutline } from "react-icons/io5";
 import "./FeedbackCard.scss";
 
-
-const FeedbackCard = () => {
+const FeedbackCard = ({ handleClose }: { handleClose: () => void }) => {
   const [step, setStep] = useState(1);
 
   return (
-    <div className="feedback-box">
-      {step === 1 && (
-        <div className="box-one">
-          <p>Your feedback matters — How do you like my portfolio?</p>
-          <div className="ratings">
-            <button onClick={() => setStep(2)}>
-              <FaRegFaceTired />
-            </button>
-            <button onClick={() => setStep(2)}>
-              <FaRegFaceFrown />
-            </button>
-            <button onClick={() => setStep(2)}>
-              <FaRegFaceMeh />
-            </button>
-            <button onClick={() => setStep(2)}>
-              <FaRegFaceSmileBeam />
-            </button>
-            <button onClick={() => setStep(2)}>
-              <FaRegFaceLaughSquint />
-            </button>
+    <div className="feedback-overlay" onClick={handleClose}>
+      <div className="feedback-box" onClick={(e) => e.stopPropagation()}>
+        <IoCloseOutline className="close-btn" onClick={handleClose} />
+        {step === 1 && (
+          <div className="box-one">
+            <p>Your feedback matters — How do you like my portfolio?</p>
+            <div className="ratings">
+              <button onClick={() => setStep(2)}>
+                <FaRegFaceTired />
+              </button>
+              <button onClick={() => setStep(2)}>
+                <FaRegFaceFrown />
+              </button>
+              <button onClick={() => setStep(2)}>
+                <FaRegFaceMeh />
+              </button>
+              <button onClick={() => setStep(2)}>
+                <FaRegFaceSmileBeam />
+              </button>
+              <button onClick={() => setStep(2)}>
+                <FaRegFaceLaughSquint />
+              </button>
+            </div>
+            <div className="labels">
+              <p>Very bad</p>
+              <p>Very good</p>
+            </div>
           </div>
-          <div className="labels">
-            <p>Very bad</p>
-            <p>Very good</p>
+        )}
+        {step === 2 && (
+          <div className="box-two">
+            <p>Please share any comments or feedback</p>
+
+            <textarea placeholder="Write here"></textarea>
+
+            <div className="buttons">
+              <button onClick={() => setStep(1)}>Back</button>
+              <button onClick={() => setStep(3)}>Submit</button>
+            </div>
           </div>
-        </div>
-      )}
-      {step === 2 && (
-        <div className="box-two">
-          <p>Please share any comments or feedback</p>
+        )}
 
-          <textarea placeholder="Write here"></textarea>
-
-          <div className="buttons">
-            <button onClick={() => setStep(1)}>Back</button>
-            <button onClick={() => setStep(3)}>Submit</button>
+        {step === 3 && (
+          <div className="box-three">
+            <p>Thank You!</p>
+            <p>Your feedback was submitted.</p>
           </div>
-        </div>
-      )}
-
-      {step === 3 && (
-        <div className="box-three">
-          <p>Thank You!</p>
-          <p>Your feedback was submitted.</p>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,43 +1,4 @@
-// import { useState } from "react";
-// import { MdFeedback } from "react-icons/md";
-// import "./Feedback.scss";
-// import FeedbackCard from "../Card/FeedbackCard/FeedbackCard";
-
-// const Feedback = () => {
-//   const [feedback, setFeedback] = useState(false);
-//   const [feedbackCard, setFeedbackCard] = useState(false);
-
-//   const showFeedback = () => {
-//     if (window.scrollY >= 800) {
-//       setFeedback(true);
-//     } else {
-//       setFeedback(false);
-//     }
-//   };
-
-//   const showFeedbackCard = () => {
-//     setFeedbackCard((prev) => !prev);
-//   };
-
-//   window.addEventListener("scroll", showFeedback);
-//   return (
-//     <>
-//       <div
-//         className={
-//           feedback ? "feedback-container active" : "feedback-container"
-//         }
-//         onClick={() => showFeedbackCard()}
-//       >
-//         <MdFeedback className="feedback-btn" />
-//       </div>
-//       {feedbackCard && <FeedbackCard />}
-//     </>
-//   );
-// };
-
-// export default Feedback;
-
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MdFeedback } from "react-icons/md";
 import "./Feedback.scss";
 import FeedbackCard from "../Card/FeedbackCard/FeedbackCard";
@@ -46,29 +7,68 @@ const Feedback = () => {
   const [feedback, setFeedback] = useState(false);
   const [feedbackCard, setFeedbackCard] = useState(false);
 
-  useEffect(() => {
-    const showFeedback = () => {
-      setFeedback(window.scrollY >= 800);
-    };
+  const showFeedback = () => {
+    if (window.scrollY >= 800) {
+      setFeedback(true);
+    } else {
+      setFeedback(false);
+    }
+  };
 
-    window.addEventListener("scroll", showFeedback);
-    showFeedback();
+  const showFeedbackCard = () => {
+    setFeedbackCard(true);
+  };
 
-    return () => window.removeEventListener("scroll", showFeedback);
-  }, []);
-
+  window.addEventListener("scroll", showFeedback);
   return (
     <>
       <div
-        className={feedback ? "feedback-container active" : "feedback-container"}
-        onClick={() => setFeedbackCard((prev) => !prev)}
+        className={
+          feedback ? "feedback-container active" : "feedback-container"
+        }
+        onClick={() => showFeedbackCard()}
       >
         <MdFeedback className="feedback-btn" />
       </div>
-
-      {feedbackCard && <FeedbackCard />}
+      {feedbackCard && <FeedbackCard handleClose={() => setFeedbackCard(false)}/>}
     </>
   );
 };
 
 export default Feedback;
+
+// import { useEffect, useState } from "react";
+// import { MdFeedback } from "react-icons/md";
+// import "./Feedback.scss";
+// import FeedbackCard from "../Card/FeedbackCard/FeedbackCard";
+
+// const Feedback = () => {
+//   const [feedback, setFeedback] = useState(false);
+//   const [feedbackCard, setFeedbackCard] = useState(false);
+
+//   useEffect(() => {
+//     const showFeedback = () => {
+//       setFeedback(window.scrollY >= 800);
+//     };
+
+//     window.addEventListener("scroll", showFeedback);
+//     showFeedback();
+
+//     return () => window.removeEventListener("scroll", showFeedback);
+//   }, []);
+
+//   return (
+//     <>
+//       <div
+//         className={feedback ? "feedback-container active" : "feedback-container"}
+//         onClick={() => setFeedbackCard((prev) => !prev)}
+//       >
+//         <MdFeedback className="feedback-btn" />
+//       </div>
+
+//       {feedbackCard && <FeedbackCard handleClose={() => setFeedbackCard(false)}/>}
+//     </>
+//   );
+// };
+
+// export default Feedback;
